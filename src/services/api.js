b4dd5b9cb5c580e62e712755a0a20278ec1dca31
Leal <102240317+alexmouraleal@users.noreply.github.com>
2024-05-02 08:02:14 -0300

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3333/",
+  baseURL: "https://heitortarefasbackend.vercel.app/",
+  //baseURL: "http://localhost:3333/",
 });
 
 //editing in git: aleal
