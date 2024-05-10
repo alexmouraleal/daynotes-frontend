4690f6049a4478bdb2f34cca11879907f66225c0
Leal <102240317+alexmouraleal@users.noreply.github.com>
2024-05-10 13:54:50 -0300

    Add files via upload

--- src/components/tasks.jsx
+++ src/components/tasks.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Button } from "@material-tailwind/react";
+import { Button, Chip, Badge, Avatar } from "@material-tailwind/react";
 import api from "../services/api";
 import Task from "./task";
 
@@ -106,6 +106,16 @@ function Tasks() {
     setOpen(dia);
   }
 
+  async function handleNovaSemana() {
+    // const deletedTask = await api.delete(`/tasks/${id}`, {});
+    // if (deletedTask.data.taskDeleted) {
+    //   setAllTasks(allTasks.filter((task) => task._id !== id));
+    // }
+    // setActionid("");
+    // setActiondia("");
+    // setOpen(dia);
+  }
+
   async function handleUpdateStatus(dia, id, status) {
     const taskStatusUpdated = await api.post(`/tasksstatus/${id}`, {
       status: status,
@@ -208,93 +218,288 @@ function Tasks() {
     }
   }
 
-  function listPoints() {
-    let tasks = 0;
-    let tasksOk = 0;
+  function listPoints(vtPoints, totTasks, totTasksC, totTasksP, totTasksN) {
+    const esperado = totTasks * 4;
+    const alcancado = totTasksC * 4 + totTasksP * 2;
+    const percsem = parseInt((alcancado * 100) / esperado);
+    let txtpercsem = "Atingiu ".concat(percsem);
+    txtpercsem = txtpercsem.concat(" %");
+    let avataropacity1 = "mr-4 avatar-opacity";
+    let avataropacity2 = "mr-4 avatar-opacity";
+    let avataropacity3 = "mr-4 avatar-opacity";
+    if (percsem >= 90) {
+      avataropacity1 = "mr-4 avatar-opacity-100";
+    }
+    if (percsem >= 70 && percsem < 90) {
+      avataropacity2 = "mr-4 avatar-opacity-100";
+    }
+    if (percsem < 70) {
+      avataropacity3 = "mr-4 avatar-opacity-100";
+    }
 
-    let tasksSeg = 0;
-    let tasksTer = 0;
-    let tasksQua = 0;
-    let tasksQui = 0;
-    let tasksSex = 0;
-    let tasksSab = 0;
-    let tasksDom = 0;
+    return (
+      <React.Fragment>
+        {vtPoints.map((data) => (
+          <div className="div-side-item marginb4">
+            <div className="flex gap-2 align-middle">
+              <Chip variant="outlined" value={data.dia} className="mr-4" />
 
-    let tasksOkSeg = 0;
-    let tasksOkTer = 0;
-    let tasksOkQua = 0;
-    let tasksOkQui = 0;
-    let tasksOkSex = 0;
-    let tasksOkSab = 0;
-    let tasksOkDom = 0;
+              <Badge color="blue" content={data.tasksC} className="mr-4">
+                <Chip color="green" value="OK" className="mr-4" />
+              </Badge>
+              <Badge color="blue" content={data.tasksP} className="mr-4">
+                <Chip color="purple" value="Parcial" className="mr-4" />
+              </Badge>
+              <Badge color="blue" content={data.tasksN} className="mr-4">
+                <Chip color="red" value="Falhou" className="mr-4" />
+              </Badge>
+            </div>
+          </div>
+        ))}
+
+        <div className="div-side-item marginb4">
+          <hr></hr>
+          <br></br>
+          Totais
+          <br></br>
+          <br></br>
+          <div className="flex gap-2 align-middle">
+            <Badge color="light-blue" content={totTasks} className="mr-4">
+              <Chip color="black" value="Tarefas" className="mr-4" />
+            </Badge>
+            <Badge color="green" content={totTasksC} className="mr-4">
+              <Chip color="black" value="Concluídas" className="mr-4" />
+            </Badge>
+            <Badge color="purple" content={totTasksP} className="mr-4">
+              <Chip color="black" value="Parcial" className="mr-4" />
+            </Badge>
+            <Badge color="red" content={totTasksN} className="mr-4">
+              <Chip color="black" value="Falhou" className="mr-4" />
+            </Badge>
+          </div>
+        </div>
+
+        <div className="div-side-item marginb4">
+          <hr></hr>
+          <br></br>
+          <div className="flex gap-2 align-middle">
+            <Badge color="light-blue" content={esperado} className="mr-4">
+              <Chip color="black" value="Esperado" className="mr-4" />
+            </Badge>
+            <Badge color="light-blue" content={alcancado} className="mr-4">
+              <Chip color="black" value="Alcançado" className="mr-4" />
+            </Badge>
+            <Chip color="blue" value={txtpercsem} className="mr-4" />
+          </div>
+        </div>
+
+        <div className="div-side-item marginb4">
+          <hr></hr>
+          <br></br>
+          <div className="flex gap-2 align-middle">
+            <Badge color="light-blue" content={esperado} className="mr-4">
+              <Chip color="black" value="Esperado" className="mr-4" />
+            </Badge>
+            <Badge color="light-blue" content={alcancado} className="mr-4">
+              <Chip color="black" value="Alcançado" className="mr-4" />
+            </Badge>
+            <Chip color="blue" value={txtpercsem} className="mr-4" />
+          </div>
+          <br></br>
+          <div className="flex gap-2 align-middle">
+            <Avatar
+              src="https://c3.klipartz.com/pngpicture/1005/48/sticker-png-meme-cry-2-crying-memes-illustration.png"
+              size="xxl"
+              className={avataropacity3}
+            />
+            <Avatar
+              src="https://www.shutterstock.com/image-photo/closeup-20-reais-banknotes-brazil-260nw-2186096491.jpg"
+              size="xxl"
+              className={avataropacity2}
+            />
+            <Avatar
+              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTTpj7RuwArFJJZTVw_StzBEtFR5FdV9wtBy1tfMsb9Pw&s"
+              size="xxl"
+              className={avataropacity1}
+            />
+          </div>
+        </div>
+
+        <div className="div-side-item marginb4">
+          <hr></hr>
+          <br></br>
+          <div className="flex gap-2 align-middle">
+            <Button
+              id="btnReset"
+              color="amber"
+              className="capitalize mt-5 w-full text-sm"
+              onClick={() => handleNovaSemana()}
+            >
+              Iniciar nova Semana
+            </Button>
+          </div>
+        </div>
+      </React.Fragment>
+    );
+  }
+
+  function getPoints() {
+    let vtPoints = [];
+    let tasksSegC = 0;
+    let tasksSegP = 0;
+    let tasksSegN = 0;
+    let tasksTerC = 0;
+    let tasksTerP = 0;
+    let tasksTerN = 0;
+    let tasksQuaC = 0;
+    let tasksQuaP = 0;
+    let tasksQuaN = 0;
+    let tasksQuiC = 0;
+    let tasksQuiP = 0;
+    let tasksQuiN = 0;
+    let tasksSexC = 0;
+    let tasksSexP = 0;
+    let tasksSexN = 0;
+    let tasksSabC = 0;
+    let tasksSabP = 0;
+    let tasksSabN = 0;
+    let tasksDomC = 0;
+    let tasksDomP = 0;
+    let tasksDomN = 0;
+    let totTasks = 0;
+    let totTasksC = 0;
+    let totTasksP = 0;
+    let totTasksN = 0;
 
     const vtSeg = allTasks.map((data) => {
       switch (data.dayexec) {
         case "SEG":
-          tasksSeg++;
           if (data.status === "C") {
-            tasksOkSeg++;
+            tasksSegC++;
           }
+          if (data.status === "P") {
+            tasksSegP++;
+          }
+          if (data.status === "N") {
+            tasksSegN++;
+          }
+          totTasks++;
           break;
         case "TER":
-          tasksTer++;
           if (data.status === "C") {
-            tasksOkTer++;
+            tasksTerC++;
           }
+          if (data.status === "P") {
+            tasksTerP++;
+          }
+          if (data.status === "N") {
+            tasksTerN++;
+          }
+          totTasks++;
           break;
         case "QUA":
-          tasksQua++;
           if (data.status === "C") {
-            tasksOkQua++;
+            tasksQuaC++;
           }
+          if (data.status === "P") {
+            tasksQuaP++;
+          }
+          if (data.status === "N") {
+            tasksQuaN++;
+          }
+          totTasks++;
           break;
         case "QUI":
-          tasksQui++;
           if (data.status === "C") {
-            tasksOkQui++;
+            tasksQuiC++;
           }
+          if (data.status === "P") {
+            tasksQuiP++;
+          }
+          if (data.status === "N") {
+            tasksQuiN++;
+          }
+          totTasks++;
           break;
         case "SEX":
-          tasksSex++;
           if (data.status === "C") {
-            tasksOkSex++;
+            tasksSexC++;
           }
+          if (data.status === "P") {
+            tasksSexP++;
+          }
+          if (data.status === "N") {
+            tasksSexN++;
+          }
+          totTasks++;
           break;
         case "SAB":
-          tasksSab++;
           if (data.status === "C") {
-            tasksOkSab++;
+            tasksSabC++;
           }
+          if (data.status === "P") {
+            tasksSabP++;
+          }
+          if (data.status === "N") {
+            tasksSabN++;
+          }
+          totTasks++;
           break;
         case "DOM":
-          tasksDom++;
           if (data.status === "C") {
-            tasksOkDom++;
+            tasksDomC++;
           }
+          if (data.status === "P") {
+            tasksDomP++;
+          }
+          if (data.status === "N") {
+            tasksDomN++;
+          }
+          totTasks++;
           break;
         default:
         // code block
       }
 
-      tasks++;
-      if (data.status === "C") {
-        tasksOk++;
-      }
+      return 0;
     });
+    vtPoints = [
+      { dia: "SEG", tasksC: tasksSegC, tasksP: tasksSegP, tasksN: tasksSegN },
+      { dia: "TER", tasksC: tasksTerC, tasksP: tasksTerP, tasksN: tasksTerN },
+      { dia: "QUA", tasksC: tasksQuaC, tasksP: tasksQuaP, tasksN: tasksQuaN },
+      { dia: "QUI", tasksC: tasksQuiC, tasksP: tasksQuiP, tasksN: tasksQuiN },
+      { dia: "SEX", tasksC: tasksSexC, tasksP: tasksSexP, tasksN: tasksSexN },
+      { dia: "SAB", tasksC: tasksSabC, tasksP: tasksSabP, tasksN: tasksSabN },
+      { dia: "DOM", tasksC: tasksDomC, tasksP: tasksDomP, tasksN: tasksDomN },
+    ];
+    totTasksC =
+      tasksSegC +
+      tasksTerC +
+      tasksQuaC +
+      tasksQuiC +
+      tasksSexC +
+      tasksSabC +
+      tasksDomC;
 
-    return (
-      <React.Fragment>
-        <div className="div-side-item">
-          <b>SEG</b> - OK ({tasksOkSeg} / {tasksSeg})
-        </div>
-        <div className="div-side-item">
-          <b>TER</b> - OK ({tasksOkTer} / {tasksTer})
-        </div>
-        <div className="div-side-item">
-          <b>GERAL</b> - OK ({tasksOk} / {tasks})
-        </div>
-      </React.Fragment>
-    );
+    totTasksP =
+      tasksSegP +
+      tasksTerP +
+      tasksQuaP +
+      tasksQuiP +
+      tasksSexP +
+      tasksSabP +
+      tasksDomP;
+
+    totTasksN =
+      tasksSegN +
+      tasksTerN +
+      tasksQuaN +
+      tasksQuiN +
+      tasksSexN +
+      tasksSabN +
+      tasksDomN;
+
+    return listPoints(vtPoints, totTasks, totTasksC, totTasksP, totTasksN);
   }
 
   return (
@@ -377,7 +582,8 @@ function Tasks() {
               Pontuação da Semana
             </AccordionHeader>
             <AccordionBody>
-              <>{listPoints()}</>
+              <>{getPoints()}</>
+              {/* <>{listPremios()}</> */}
             </AccordionBody>
           </Accordion>
         </div>
